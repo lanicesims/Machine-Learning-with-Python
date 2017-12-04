13f3d4f97e052a44c4402baf4cdedbda098a56b9
Lanice <lanice.sims@gmail.com>
2017-12-03 20:14:51 -0600

    Define a feature and edit notes

--- intro-regression.py
+++ intro-regression.py
@@ -5,18 +5,18 @@ import quandl
 #dataframe equals df
 df = quandl.get('WIKI/GOOGL')
 
-#print(df.head) - this will call all the features. 
+#print(df.head) - this will call your columns/features. 
 
-#But we want to determine which features are useful/meaningful and which aren't. To make a list of the features that we want.
+#We want to determine which features are useful/meaningful and which aren't. To make a list of the features that we want, we have the columns Open, High, Low, Close, and Volume:
 
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume',]]
 
-#dataframe will now only have the columns Open, High, Low, Close, and Volume. Now let's create relationships between the different features.
+# Let's create relationships between some of the different features.
 
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0 #This will give is the margin of change in percent form. 
 
-df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0 
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0 #This will give the daily percent change.
 
-df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']] #This defines a new dataframe with only the columns/features we care about.
 
 print(df.head())
