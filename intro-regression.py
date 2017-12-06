610a55e3e49a71e8a4d59d2a7d17ede4bb5fc01d
Lanice <lanice.sims@gmail.com>
2017-12-05 20:45:13 -0600

    Define features and labels. Then pass through classifier, and train and test classifier.

--- intro-regression.py
+++ intro-regression.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import quandl
 import math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 #dataframe equals df
 df = quandl.get('WIKI/GOOGL')
@@ -29,16 +32,48 @@ forecast_col = 'Adj. Close'
 
 df.fillna(-99999, inplace=True) 
 
-#NA refers to missing or not available data. In the real world, one can miss data (say a column is missing). In machine learning, we can't work with nan data, so the nan data has to be replaced with something. When you call 'df.fillna', this data will be included but treated like an outlier in dataset.   
+#NA refers to missing or not available data. With machine learning, we will replace any NaN values with a real number (even though it's an #outlier). This is a better choice than getting rid of columns that don't have all the data 
+#
+# We can't work with nan data, so the nan data has to be replaced with something. When you call 'df.fillna', this data will be included but #treated like an outlier in dataset.   
 
-#To forecast out: 
+#To forecast out (predict future values): 
 
-forecast_out = int(math.ceil(.01*len(df))) 
+forecast_out = int(math.ceil(.01*len(df)))
 
-#math.ceil rounds everything up. We are trying to predict out 1% out.
+print(forecast_out) 
+
+#math.ceil rounds everything up. We are trying to predict out 1% out (depending on the number of days, that many days * .01 out).
 
 df['label'] = df[forecast_col].shift(-forecast_out) 
+df.dropna(inplace=True)
 
 #.shift is pandas method that is used to shift the column in a direction by any number you decide. This new column will be trained against. Each row will be the Adj. Close in 10%
 
-print(df.head())
+#print(df.head())
+
+#define X and y (generally features is represented as a capital X and labels as a lowercase y)
+
+X = np.array(df.drop(['label'],1)) #df.drop will return a new dataframe with all the features but the label column
+y = np.array(df['label'])
+
+#now we want to scale X. Need to scale before putting it through a classifier.
+
+X = preprocessing.scale(X) #using it on real time on real data
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+#take all features and labels (keeping Xs and ys together) and will output X and y training data and X and y testing data. We use X_train and y_train to fit our classifiers
+
+#we are finding a classifier by choosing SKLearn's Linear Regression algorithnm for our ML example. We can change out clf = LinearRegression() to easily to use a different algorithm.
+
+clf = LinearRegression()
+#clf = svm.SVR()  this is support vector regression.
+
+clf.fit(X_train, y_train) # using clf.fit to fit or train a classifier; you fit features and labels.
+
+#now we have our classifier to use and predict data
+
+accuracy = clf.score(X_test, y_test) #testing the accuracy of our classifier and see how well the training set compares to the testing data.
+
+print(accuracy)
