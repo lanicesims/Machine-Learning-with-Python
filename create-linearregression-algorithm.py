b6aca7fd0828388582b2c7a41835fb040e1ce003
Lanice <lanice.sims@gmail.com>
2017-12-10 18:53:43 -0600

    Program coefficient of determination

--- create-linearregression-algorithm.py
+++ create-linearregression-algorithm.py
@@ -19,14 +19,34 @@ def best_fit_slope_and_yint(xs,ys):
     b = mean(ys) - m * mean(xs)
     return m, b
 
-m, b = best_fit_slope_and_yint(xs,ys)
+#How good is our best fit line? We can use the coefficient of determination to find out. Using a new function that squares error.
 
-print(m, b)
+def squared_error(ys_orig, ys_line): #the distance from the y point and the line in question is the error, then we square it. 
+    return sum((ys_line - ys_orig)**2)
+
+def coefficient_of_determination(ys_orig, ys_line):
+    y_mean_line = [mean(ys_orig) for y in ys_orig]
+    squared_error_regr = squared_error(ys_orig, ys_line)
+    squared_error_y_mean = squared_error(ys_orig, y_mean_line)
+    return 1 - (squared_error_regr / squared_error_y_mean)
+
+m, b = best_fit_slope_and_yint(xs,ys)
 
 #We can create a line that fits the data we have.
 
 regression_line = [(m*x + b) for x in xs]
 
+#To find out the r-squared value of our regression line:
+
+r_squared = coefficient_of_determination(ys, regression_line)
+print(r_squared)
+
+#We can use the best fit line to predict values of y.
+
+predict_x = 8
+predict_y = (m * predict_x) + b
+
 plt.scatter(xs, ys)
+plt.scatter(predict_x, predict_y, color='green')
 plt.plot(xs, regression_line)
 plt.show()
