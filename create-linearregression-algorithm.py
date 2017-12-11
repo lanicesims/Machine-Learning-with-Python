3aa2a75aa4a32c691a2276984630d4a38a77b591
Lanice <lanice.sims@gmail.com>
2017-12-10 19:38:12 -0600

    Test assumptions by creating random dataset.

--- create-linearregression-algorithm.py
+++ create-linearregression-algorithm.py
@@ -5,11 +5,27 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import random
 
 style.use('fivethirtyeight')
 
-xs = np.array([1,2,3,4,5,6], dtype=np.float64)
-ys = np.array([5,4,6,5,6,7], dtype=np.float64)
+# xs = np.array([1,2,3,4,5,6], dtype=np.float64)
+# ys = np.array([5,4,6,5,6,7], dtype=np.float64)
+
+#We want to be able to test our algorithm and r-squared value. So we can create a dataset by randomly assign numbers in our dataset using the specific parameters.
+
+def create_dataset(hm, variance, step=2, correlation=False):
+    val = 1
+    ys = []
+    for i in range(hm):
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+        if correlation and correlation == 'pos':
+            val += step
+        elif correlation and correlation == 'neg':
+            val -= step
+        xs = [i for i in range(len(ys))]
+    return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
 
 #calculation of the slope (m) and y-int (b)
 
@@ -29,6 +45,8 @@ def coefficient_of_determination(ys_orig, ys_line):
     squared_error_regr = squared_error(ys_orig, ys_line)
     squared_error_y_mean = squared_error(ys_orig, y_mean_line)
     return 1 - (squared_error_regr / squared_error_y_mean)
+
+xs, ys = create_dataset(40, 6, 2, correlation='neg')
 
 m, b = best_fit_slope_and_yint(xs,ys)
 
