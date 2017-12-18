2bb16a1db656f49bdf06065ac5fa4a4d19ab9985
Lanice <lanice.sims@gmail.com>
2017-12-17 18:25:50 -0600

    Use KNearest Neighbor algorithm to test real world data with breast cancer data

--- create_KNearestNeighbor_algorithm.py
+++ create_KNearestNeighbor_algorithm.py
@@ -1,10 +1,9 @@
 import numpy as np 
-from math import sqrt
-import matplotlib.pyplot as plt 
+from math import sqrt 
 import warnings
-from matplotlib import style
+import pandas as pd 
 from collections import Counter
-style.use('fivethirtyeight')
+import random
 
 #calculate euclidean distance
 
@@ -13,8 +12,8 @@ style.use('fivethirtyeight')
 
 # euclidean_distance = sqrt( (plot1[0]-plot2[0])**2 + (plot1[1]-plot2[1])**2 ) Unfortunately this isn't a fast calculation so isn't entiely useful.
 
-dataset = {'k': [[1,2], [2,3], [3,1]], 'r':[[6,5], [7,7], [8,6]]} #Created two classes and their features.
-new_features = [5,7]
+# dataset = {'k': [[1,2], [2,3], [3,1]], 'r':[[6,5], [7,7], [8,6]]} #Created two classes and their features.
+# new_features = [5,7]
 
 #Loop that graphs the dataset and new_features.
 
@@ -43,8 +42,45 @@ def k_nearest_neighbors(data, predict, k=3):
             distances.append([euclidean_distance, group]) 
     votes = [i[1] for i in sorted(distances)[:k]]
     vote_result = Counter(votes).most_common(1)[0][0]
-    print(Counter(votes).most_common(1))
+    #print(Counter(votes).most_common(1))
     return vote_result
 
-result = k_nearest_neighbors(dataset, new_features, k=3)
-print(result)
+# result = k_nearest_neighbors(dataset, new_features, k=3)
+# print(result)
+
+#Testing k_nearest neighbors on real world data. Want to compare our accuracy to scikit learn's accuracy.
+
+df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+full_data = df.astype(float).values.tolist() #want to convert data into float
+
+#shuffle data
+
+random.shuffle(full_data)
+
+test_size = 0.2
+train_set = {2:[], 4:[]}
+test_set = {2:[], 4:[]}
+train_data = full_data[:-int(test_size*len(full_data))] #first 20% of data
+test_data = full_data[-int(test_size*len(full_data)):] #last 20% of data
+
+# Shuffle and sliced data. Now we want to populate dictionaries.
+
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
+
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
+
+correct = 0
+total = 0
+
+for group in test_set:
+    for data in test_set[group]:
+        vote = k_nearest_neighbors(train_set, data, k=5)
+        if group == vote:
+            correct += 1
+        total += 1
+
+print('Accuracy:', correct/total)
